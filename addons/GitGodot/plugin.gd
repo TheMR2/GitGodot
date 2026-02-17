99ef503c499863be60fb296eafd3a612a6ab4fdb
Mohamed Rajab <mr8187649@gmail.com>
2026-02-17 15:34:28 +0200

    GitGodot V3.0

--- addons/GitGodot/plugin.gd
+++ addons/GitGodot/plugin.gd
@@ -6,12 +6,20 @@ var dock:EditorDock
 
 func _enter_tree() -> void:
 	dock = EditorDock.new()
-	dock.name = "GitGodot 2.0"
+	dock.name = "GitGodot 3"
 	dock.default_slot = EditorDock.DOCK_SLOT_BOTTOM
-	var dock_content = preload("res://addons/GitGodot/GitGodot.tscn").instantiate()
-	dock.add_child(dock_content)
+	if Check_Git():
+		var dock_content = preload("res://addons/GitGodot/GitGodot.tscn").instantiate()
+		dock.add_child(dock_content)
+	else:
+		var dock_content = preload("res://addons/GitGodot/No_Git.tscn").instantiate()
+		dock.add_child(dock_content)
 	add_dock(dock)
 
 func _exit_tree() -> void:
 	remove_dock(dock)
 	dock.queue_free()
+	
+func Check_Git() -> bool:
+	return DirAccess.dir_exists_absolute("res://.git")
+	pass
